a3ebc24965f0da5a03dae8ccaeba35fb5a3a8c5e
pathum177 <pathummalsara177@gmail.com>
2025-08-11 19:31:06 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙴𝙻𝚄𝙼𝙸-𝙼𝙳=put your session_id' : process.env.SESSION_ID,
+PREFIX: process.env.PREFIX || '.' ,
+MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
+};
