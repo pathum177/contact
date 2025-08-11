172b214828b73e9a4f34b4710bdf45080c1a7b2b
pathum177 <pathummalsara177@gmail.com>
2025-08-11 19:35:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙴𝙻𝚄𝙼𝙸-𝙼𝙳=put your session_id' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=IQZ0hQJD#85OPfjGWZs2KqXHUYpCOF3dyPShl6m_Y9wXs7Te9GHc' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
