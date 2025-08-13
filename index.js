1e18edc77cde519b1889d731f61709f5bc186246
pathum177 <pathummalsara177@gmail.com>
2025-08-13 20:27:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ const {
   sleep, 
   fetchJson 
 } = require('./lib/functions')
-const { AntiDelDB, initializeAntiDeleteSettings, setAnti, getAnti, getAllAntiDeleteSettings, saveContact, loadMessage, getName, getChatSummary, saveGroupMetadata, getGroupMetadata, saveMessageCount, getInactiveGroupMembers, getGroupMembersMessageCount, saveMessage } = require('./data')
 const fs = require('fs')
 const P = require('pino')
 const FileType = require('file-type')
