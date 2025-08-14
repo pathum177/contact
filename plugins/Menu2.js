e3c42edbd73c6b7408965bfd4ab977a767545897
pathum177 <pathummalsara177@gmail.com>
2025-08-14 15:02:46 +0530

    Update Menu2.js

--- plugins/Menu2.js
+++ plugins/Menu2.js
@@ -1,7 +1,6 @@
 const { cmd, commands } = require('../lib/command')
 const config = require('../settings')
-const os = require('os')
-var { get_set , input_set } = require('../lib/set_db') 
+const os = require('os') 
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
 
 
