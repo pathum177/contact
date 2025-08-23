d17b335692bc0ffcdccaa921167f2298cade7b54
pathum177 <pathummalsara177@gmail.com>
2025-08-24 00:22:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=IQZ0hQJD#85OPfjGWZs2KqXHUYpCOF3dyPShl6m_Y9wXs7Te9GHc' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=VIIxyRYC#gnTjHnJnOd1COuDlxoBs8xCii2yM7TzPNbi4TYpBiS8' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"groups" : process.env.MODE,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS
