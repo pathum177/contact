bb44c8a5f86d842f7d8286b40b33a675af2b31d7
pathum177 <pathummalsara177@gmail.com>
2025-09-16 01:34:44 +0530

    Update ai2.js

--- plugins/ai2.js
+++ plugins/ai2.js
@@ -1,5 +1,5 @@
-const config = require('../config')
-const {cmd , commands} = require('../command')
+const config = require('../settings')
+const {cmd , commands} = require('../lib/command')
 const { fetchJson } = require('../lib/functions')
 
 cmd({
